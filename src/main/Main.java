0ee9537d60de26f4fb24c4858a06de491d19c394
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-09-27 09:56:16 +0700

    add files

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,12 @@ import core.MulticastApp;
 import parser.NetworkParser;
 
 public class Main {
-    public static void main(String[] args) {
-    	new MulticastApp(new NetworkParser().getNetworkContext("config.properties"));
+    public static void main(String[] args)  {
+    	try {
+			new MulticastApp(new NetworkParser().getNetworkContext("config.properties"));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    
     }
 }
-
