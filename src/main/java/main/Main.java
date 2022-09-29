8d8b8c52b2385454732bc6249148fae6e1d7166d
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-09-29 17:38:03 +0700

    fix

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main.java.main;
+
+import main.java.core.MulticastApp;
+import main.java.parser.NetworkParser;
+
+public class Main {
+    public static void main(String[] args)  {
+    	 try {
+    		   if (args.length < 1) {
+              System.out.println("Not enough arguments\nYou should type ip address");    
+              System.exit(0);
+           }    		
+			     new MulticastApp(new NetworkParser().getNetworkContext("/config.properties", args[0]));
+		   } catch (InterruptedException e) {
+			      e.printStackTrace();
+		  }    
+    }
+}
+
